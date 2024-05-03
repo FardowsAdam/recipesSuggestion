bbbb783b3da37210b57bebeb120b7b724d6a2b15
FardowsAdam <166836087+FardowsAdam@users.noreply.github.com>
2024-05-03 19:22:00 +0300

    Add files via upload

---
+++ Dinner.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Dinner extends Meal{
+    Map<String, Recipes> map=new HashMap<>();
+    public Dinner() {
+    }
+}
---
+++ Lunch.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Lunch extends Meal{
+    Map<String, Recipes> map=new HashMap<>();
+    public Lunch() {
+    }
+}
