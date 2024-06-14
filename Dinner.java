6907aef249044d55c8a062438d73103e934ad75c
FardowsAdam <166836087+FardowsAdam@users.noreply.github.com>
2024-06-14 16:23:31 +0300

    Delete Dinner.java

--- Dinner.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class Dinner extends Meal{
-    Map<String, Recipes> map=new HashMap<>();
-    public Dinner() {
-    }
-}
