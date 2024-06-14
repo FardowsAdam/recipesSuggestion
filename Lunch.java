2b3ccfaeb32d9a0052de7ce24d5ef2ece9fd0341
FardowsAdam <166836087+FardowsAdam@users.noreply.github.com>
2024-06-14 16:23:45 +0300

    Delete Lunch.java

--- Lunch.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class Lunch extends Meal{
-    Map<String, Recipes> map=new HashMap<>();
-    public Lunch() {
-    }
-}
